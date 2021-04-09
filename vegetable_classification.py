7d0416d9812682c7447942591b496653c57d87a6
pankajrajput0312 <pankajrajput020010@gmail.com>
2021-04-09 16:42:30 +0530

    weight_detection file

--- vegetable_classification.py
+++ vegetable_classification.py
@@ -9,9 +9,7 @@ from msrest.authentication import ApiKeyCredentials
 credentials = ApiKeyCredentials(in_headers = {"Prediction-Key":"bf595a2cb1854d988a1f9d26834cd4e2"})
 predictor =  CustomVisionPredictionClient("https://pankaj.cognitiveservices.azure.com/", credentials)
 
-# ret, image = camera.read()
-# cv2.imwrite('capture.png', image)
-# camera.release()
+
 with open("capture.png", mode ='rb') as captured_image:
     print("load image... and predict ")
     results = predictor.classify_image("c22b7174-dd80-457b-829e-4d05496aee33", "Iteration3", captured_image)
--- weight_data.py
+++
@@ -1,2 +0,0 @@
-def weight(count):
-    return count
